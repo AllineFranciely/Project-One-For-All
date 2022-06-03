4085219b624da98ee4ef598b3308a4664fca20af
Alline <alline_franciely@hotmail.com>
2022-06-03 16:05:48 -0300

    altera ordem requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 SELECT 
 	COUNT(can.song_id) AS cancoes,
-    COUNT(DISTINCT alb.album_id) AS albuns,
-    COUNT(DISTINCT art.artist_id) AS artistas
+    COUNT(DISTINCT art.artist_id) AS artistas,
+    COUNT(DISTINCT alb.album_id) AS albuns
 FROM SpotifyClone.song AS can
 INNER JOIN SpotifyClone.album AS alb
 ON can.album_id = alb.album_id
