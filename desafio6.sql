c0beb1f0e1eccb8d2237b6b8479d46f6e1d3e6c1
Alline <alline_franciely@hotmail.com>
2022-06-03 19:25:30 -0300

    6 pra variar

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-	ROUND(MIN(val.value), 2) AS 'faturamento-minimo',
-    ROUND(MAX(val.value), 2) AS 'faturamento_maximo',
-    ROUND(AVG(val.value), 2) AS 'faturamento-medio',
-    ROUND(SUM(val.value), 2) AS 'faturamento-total'
+  ROUND(MAX(val.value), 2) AS faturamento_maximo,
+  ROUND(AVG(val.value), 2) AS faturamento_medio,
+  ROUND(MIN(val.value), 2) AS faturamento_minimo,
+  ROUND(SUM(val.value), 2) AS faturamento_total
 FROM SpotifyClone.plan AS val
 INNER JOIN SpotifyClone.users AS u
-ON val.plan_id = u.plan_id; 
+ON val.plan_id = u.plan_id;
